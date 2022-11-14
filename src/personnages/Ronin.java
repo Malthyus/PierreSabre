67aec3e464dd2764142c821857fa5ffb41dfca3b
Malthyus <114723838+Malthyus@users.noreply.github.com>
2022-11-15 00:53:36 +0100

    duel entre Ronin et Yakuza

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -8,7 +8,23 @@ public class Ronin extends Humain {
 	
 	public void donner(Commercant beneficiaire) {
 		int arg=nbArgent/10;
+		perdreArgent(arg);
 		parler(beneficiaire.getNom()+" prend ces "+arg+" sous.");
 		beneficiaire.recevoirArgent(arg);
+	}
+	
+	public void provoquer(Yakuza adversaire) {
+		int force=honneur*2;
+		parler("Je t'ai retrouvé vermine, tu vas payer pour ce que tu as fait à ce pauvre marchand!");
+		if (force>=adversaire.getReputation()) {
+			parler("Je t'ai eu petit yakusa!");
+			gagnerArgent(adversaire.perdre());
+			honneur++;
+		} else {
+			parler("J'ai perdu contre ce yakuza, mon honneur et ma bourse ont en pris un coup.");
+			adversaire.gagner(nbArgent);
+			perdreArgent(nbArgent);
+			honneur-=1;
+		}
 	}
 }
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -8,6 +8,10 @@ public class Yakuza extends Humain {
 		this.clan=clan;
 	}
 	
+	public int getReputation() {
+		return reputation;
+	}
+
 	public void extorquer(Commercant victime) {
 		parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
 		parler(victime.getNom()+", si tu tiens à la vie donne moi ta bourse !");
@@ -15,5 +19,19 @@ public class Yakuza extends Humain {
 		gagnerArgent(arg);
 		reputation++;
 		parler("J’ai piqué les "+arg+" sous de "+victime.getNom()+", ce qui me fait "+nbArgent+" sous dans ma poche. Hi ! Hi !");
+	}
+	
+	public int perdre() {
+		int arg=nbArgent;
+		perdreArgent(nbArgent);
+		reputation-=1;
+		parler("J'ai perdu mon duel et mes "+arg+" sous, snif... J'ai déshonoré le clan de "+clan+".");
+		return arg;
+	}
+	
+	public void gagner(int gain) {
+		gagnerArgent(gain);
+		reputation++;
+		parler("Ce ronin pensait vraiment battre "+getNom()+" du clan "+clan+" ? Je l'ai dépouillé de ses "+gain+" sous.");
 	}
 }
