15c0de5b5691df163a5cfd144a1d65ff91431d7e
Malthyus <114723838+Malthyus@users.noreply.github.com>
2022-11-15 09:11:22 +0100

    les humains ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,14 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Humain {
+	private static final int NB_CONNAISSANCE_MAX=30;
 	private String nom;
 	private String boissonFav;
 	protected int nbArgent;
+	protected int nbConnaissance=0;
+	protected Humain[] memoire= new Humain[NB_CONNAISSANCE_MAX];
 	public Humain(String nom, String boissonFav, int nbArgent) {
 		super();
 		this.nom = nom;
@@ -43,5 +48,41 @@ public class Humain {
 	
 	protected void parler(String texte) {
 		System.out.println(texte);
+	}
+	
+	public void faireConnaissance(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre(this);
+		memoriser(autreHumain);
+	}
+	private void memoriser(Humain autreHumain) {
+		if (nbConnaissance<NB_CONNAISSANCE_MAX) {
+			memoire[nbConnaissance]=autreHumain;
+			nbConnaissance++;
+		} else {
+			int i=0;
+			while (i<NB_CONNAISSANCE_MAX-1) {
+				memoire[i]=memoire[i+1];
+				i++;
+			}
+			memoire[i]=autreHumain;
+		}
+		
+	}
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+		
+	}
+	
+	public void listerConnaissance() {
+		String texte= "Je connais beaucoup de monde dont:";
+		for (int i = 0; i < nbConnaissance-1; i++) {
+			texte+=memoire[i].getNom()+",";
+		}
+		if (nbConnaissance!=0) {
+			texte+=memoire[nbConnaissance-1].getNom();
+		}
+		parler(texte);
 	}
 }
